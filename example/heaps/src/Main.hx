e6e748ab2265116cec1ef3f1e52266ff667fb891
Endel Dreyer <endel.dreyer@gmail.com>
2024-01-18 09:48:24 -0300

    use latest API on heaps example

--- example/heaps/src/Main.hx
+++ example/heaps/src/Main.hx
@@ -6,29 +6,29 @@ class Main extends hxd.App {
   private var room: Room<State>;
 
 	override function init() {
-    this.client.joinOrCreate("test", [], State, function(err, room) {
-      if (err != null) {
-        trace(err);
-        return;
-      }
+		this.client.joinOrCreate("test", [], State, function(err, room) {
+			if (err != null) {
+				trace(err);
+				return;
+			}
 
 			// this triggers only when map or array are created with `new`
 			// when new value appended it is also triggered but traces just empty array
-			room.state.container.onChange = function(v) trace('Root.onChange', v);
+			room.state.container.onChange(function(v) trace('Root.onChange', v));
 
 			// following callbacks are never triggered
-			room.state.container.testMap.onChange = function(v, k) trace('Map.onChange', v, 'key', k);
-			room.state.container.testMap.onAdd = function(v, k) trace('Map.onAdd', v, 'key', k);
-      room.state.container.testMap.onRemove = function(v, k) trace('Map.onRemove', v, 'key', k);
+			room.state.container.testMap.onChange(function(v, k) trace('Map.onChange', v, 'key', k));
+			room.state.container.testMap.onAdd(function(v, k) trace('Map.onAdd', v, 'key', k));
+			room.state.container.testMap.onRemove(function(v, k) trace('Map.onRemove', v, 'key', k));
 
-			room.state.container.testArray.onChange = function(v, k) trace('Array.onChange', v, 'key', k);
-			room.state.container.testArray.onAdd = function(v, k) trace('Array.onAdd', v, 'key', k);
-			room.state.container.testArray.onRemove = function(v, k) trace('Array.onRemove', v, 'key', k);
+			room.state.container.testArray.onChange(function(v, k) trace('Array.onChange', v, 'key', k));
+			room.state.container.testArray.onAdd(function(v, k) trace('Array.onAdd', v, 'key', k));
+			room.state.container.testArray.onRemove(function(v, k) trace('Array.onRemove', v, 'key', k));
 
-      this.room = room;
-    });
+			this.room = room;
+		});
 
-    var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+		var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
 		tf.text = "Hello World !";
 	}
 
