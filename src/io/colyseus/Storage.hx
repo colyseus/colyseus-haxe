c8364e03bd87144849319f4f331fb5943878445f
Endel Dreyer <endel.dreyer@gmail.com>
2025-02-28 09:27:16 -0300

    AirSDK: fix cache/storage support via flash.net.SharedObject

--- src/io/colyseus/Storage.hx
+++ src/io/colyseus/Storage.hx
@@ -2,6 +2,10 @@ package io.colyseus;
 
 import tink.core.Future;
 
+#if (flash)
+import flash.net.SharedObject;
+#end
+
 class Storage {
     public static var PATH = "colyseus-storage";
     private static var inmemoryKV: Map<String, String> = new Map<String,String>();
@@ -50,7 +54,41 @@ class Storage {
 		return storage.removeItem(PATH + ":" + name);
 	}
 
-	#else
+	#elseif (flash)
+
+	// Private helper to get the SharedObject instance
+	private static function getSharedObject():SharedObject {
+		return SharedObject.getLocal(PATH);
+	}
+
+	private static function getData(name:String):String {
+		var so:SharedObject = getSharedObject();
+		// Check if the key exists in the SharedObject's data
+
+		if (Reflect.hasField(so.data, name)) {
+			return Reflect.getProperty(so.data, name);
+		}
+		return null;
+	}
+
+	private static function setData(name:String, value:String):Void {
+		var so:SharedObject = getSharedObject();
+		// Set the value for the given name (key)
+		Reflect.setProperty(so.data, name, value);
+		// Save the data to disk
+		so.flush();
+	}
+
+	private static function removeData(name:String):Void {
+		var so:SharedObject = getSharedObject();
+		// Remove the specific key if it exists
+		if (Reflect.hasField(so.data, name)) {
+			Reflect.deleteField(so.data, name);
+			so.flush(); // Persist the change
+		}
+	}
+
+    #else
 
 	private static function getData(name:String)
 	{
